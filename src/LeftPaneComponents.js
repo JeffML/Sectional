d20c9416a04ce39868e4c40fa90a5dd611948d40
Jeff Lowery <jlowery2663@gmail.com>
2018-01-01 13:34:31 -0800

    simple grid

--- src/LeftPaneComponents.js
+++ src/LeftPaneComponents.js
@@ -2,78 +2,51 @@ import React, {Component} from 'react'
 
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
-import ReactGridLayout from 'react-grid-layout'
+import 'react-simple-flex-grid/lib/main.css'
+import {Row, Col} from 'react-simple-flex-grid'
 
 var buttonStyle = {
   margin: '10px 10px 10px 0'
 };
 
-const hostState = {inputHost: null}
-
-var layout = [
-  {i: 'select', x: 0, y: 0, w: 8, h: 20, static: true},
-  {i: 'use', x: 8, y: 0, w: 2, h: 20},
-  {i: 'delete',  x: 10, y: 0, w: 2, h: 20}
-];
-
-const panelStyle = {backgroundColor: "violet"}
+const hostState = {
+  inputHost: null
+}
 
 class SelectPanel extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
-    const props = {...this.props}
+    const props = {
+      ...this.props
+    }
 
-    return (
-      <ReactGridLayout className="layout" style={panelStyle} layout={layout} cols={12} rowHeight={30} width={385}>
-        <div key="select">
-          <HostSelect {...props}/>
-        </div>
-        <div key="use">
-          <HostUse/>
-        </div>
-        <div key="delete"><HostDelete/></div>
-      </ReactGridLayout>
-    )
+    return (<Row>
+      <Col span={7}><HostSelect {...props}/></Col>
+      <Col span={3}><HostUse {...props}/></Col>
+      <Col span={2}><HostDelete {...props}/></Col>
+    </Row>)
   }
 }
 
+const HostSelect = (props) => (<Select name="form-field-name" placeholder="Select CouchDB Host" value={props.selectedHost && props.selectedHost.value} onChange={props.handleHostSelect} options={props.knownHosts}/>)
 
-const HostSelect = (props) => (
-  <Select
-    name="form-field-name"
-    placeholder="Select CouchDB Host"
-    value={props.currHost.value}
-    onChange={props.handleHostChange}
-    options={props.knownHosts}
-  />
-)
+const HostInput = (props) => (<input type='text' ref={input => {
+    hostState.inputHost = input
+  }}/>)
 
-const HostInput = (props) => (
-  <input type='text' ref={input => {hostState.inputHost = input;}}  />
-)
+const HostAdd = (props) => (<button className="btn btn-default" style={buttonStyle} onClick={() => {
+    props.handleHostAdd({value: hostState.inputHost.value, label: hostState.inputHost.value})
+  }}>Add Host</button>)
 
-const HostAdd = (props) => (
-  <button
-        className="btn btn-default"
-        style={buttonStyle}
-        onClick={() => {props.handleHostAdd({value: hostState.inputHost.value, label: hostState.inputHost.value})}}>Add Host</button>
-)
+const HostUse = (props) => (<button className="btn btn-default" style={buttonStyle} onClick={() => {
+    props.handleHostUse({value: props.selectedHost.value, label: props.selectedHost.value})
+  }}>Use</button>)
 
-const HostUse = (props) => (
-  <button
-        className="btn btn-default"
-        style={buttonStyle}
-        onClick={() => {props.handleHostAdd({value: hostState.inputHost.value, label: hostState.inputHost.value})}}>Use</button>
-)
+const HostDelete = (props) => (<button className="btn" style={buttonStyle} onClick={() => {
+    props.handleHostDelete({value: props.selectedHost.value, label: props.selectedHost.value})
+  }}>Delete</button>)
 
-const HostDelete = (props) => (
-  <button
-        className="btn btn-default"
-        style={buttonStyle}
-        onClick={() => {props.handleHostAdd({value: hostState.inputHost.value, label: hostState.inputHost.value})}}>Delete</button>
-)
-
-export {HostInput, HostAdd, SelectPanel}
+export {
+  HostInput,
+  HostAdd,
+  SelectPanel
+}
--- src/MainGrid.js
+++ src/MainGrid.js
@@ -1,14 +1,15 @@
-import ReactGridLayout from 'react-grid-layout'
 import React, {Component} from 'react';
 import {LeftPane, RightPane} from './Panes'
 import nano from 'nano'
+import 'react-simple-flex-grid/lib/main.css'
+import {Row, Col} from 'react-simple-flex-grid'
 
 class MainGrid extends Component {
   constructor(props) {
     super(props)
 
-    if (typeof (Storage) === "undefined") {
-        alert("Sorry, but this application requires browser support for Web Storage")
+    if (typeof(Storage) === "undefined") {
+      alert("Sorry, but this application requires browser support for Web Storage")
     }
 
     this.state = {
@@ -26,7 +27,7 @@ class MainGrid extends Component {
 
   render() {
     const handleHostSelect = (selectedHost) => {
-      this.setState({ selectedHost });
+      this.setState({selectedHost});
       console.log(`Selected: ${selectedHost.label}`);
     }
 
@@ -35,34 +36,54 @@ class MainGrid extends Component {
       try {
         const cx = nano(currHost.value)
         this.setState({cx})
-        this.setState({knownHosts: this.state.knownHosts.concat([currHost])})
-        this.setState({ currHost });
+        this.setState({
+          knownHosts: this.state.knownHosts.concat([currHost])
+        })
+        this.setState({currHost});
         console.log(`Added: ${currHost.label}`);
-      } catch(e) {
+      } catch (e) {
         alert(`Unable to connect to ${currHost.value}`)
       }
     }
 
+    const handleHostUse = (currHost) => {
+      //check connection
+      try {
+        const cx = nano(currHost.value)
+        this.setState({cx})
+        this.setState({currHost});
+        console.log(`Using: ${currHost.label}`);
+      } catch (e) {
+        alert(`Unable to connect to ${currHost.value}`)
+      }
+    }
+
+    const handleHostDelete = (currHost) => {
+      //check connection
+      this.setState({
+        knownHosts: this.state.knownHosts.filter(host => host.value !== currHost.value)
+      })
+      console.log(`Deleted: ${currHost.label}`);
+    }
+
     const props = {
       handleHostSelect: handleHostSelect.bind(this),
-      handleHostAdd: handleHostAdd.bind(this)
+      handleHostAdd: handleHostAdd.bind(this),
+      handleHostUse: handleHostUse.bind(this),
+      handleHostDelete: handleHostDelete.bind(this)
     }
 
     Object.assign(props, this.props, this.state)
 
-    // layout is an array of objects, see the demo for more complete usage
-    var layout = [
-      {i: 'left', x: 0, y: 0, w: 4, h: 20, static: true},
-      {i: 'right', x: 5, y: 0, w: 8, h: 20}
-    ];
-
-    return (
-      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+    return (<Row>
+      <Col span={4}>
         <div key="left"><LeftPane {...props}/></div>
-        <div key="right"><RightPane/></div>
-      </ReactGridLayout>
-    )
-  }
+      </Col>
+      <Col span={8}><RightPane/></Col>
+    </Row>)
+  };
 };
 
-export {MainGrid};
+export {
+  MainGrid
+};
